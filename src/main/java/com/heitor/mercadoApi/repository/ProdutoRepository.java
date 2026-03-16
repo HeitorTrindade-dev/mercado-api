b98b85c8362aa0641378fbe2cce47c827bae3eee
HeitorTrindade-dev <heitortrindade-dev@gmail.com>
2026-03-16 15:48:25 -0300

    Começando sql

--- src/main/java/com/heitor/mercadoApi/repository/ProdutoRepository.java
+++ src/main/java/com/heitor/mercadoApi/repository/ProdutoRepository.java
@@ -1,8 +1,10 @@
 package com.heitor.mercadoApi.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
@@ -11,10 +13,23 @@ import com.heitor.mercadoApi.model.Produto;
 
 @Repository
 public class ProdutoRepository {
+
+    @Autowired
+    private DataSource dataSource;
+
     public void insertProduto(Produto produto) throws SQLException{
         String scriptSql = "INSERT INTO produtos(nome,preco,quantdade) VALUES (?,?,?)";
 
-        Connection connection = DataSource.getConne
+        Connection connection = dataSource.getConnection();
+
+        PreparedStatement statement = connection.prepareStatement(scriptSql);
+
+        statement.setString(1,produto.getNome());
+        statement.setDouble(2, produto.getPreco());
+        statement.setInt(3, produto.getQuantidade());
+
+
+        statement.executeUpdate();
 
     }
 }
